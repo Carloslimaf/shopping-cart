40adf195f10e87568b83721d581d48e0aab37010
CarlosLima <carlos.augustolimaf@gmail.com>
2021-07-12 18:54:21 -0300

    Adiciona o 3 requisito

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener() {
-  // coloque seu código aqui
+function cartItemClickListener(event) {
+ event.target.remove();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
